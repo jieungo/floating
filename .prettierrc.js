4eda90cf143bdf7e0d2ccaf6068d7d9b96e7879a
jieungo <jieungogogo@gmail.com>
2022-12-09 16:26:26 +0900

    :heavy_plus_sign: Add: 라이브러리 및 prettierrc 설정 추가

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,28 @@
 module.exports = {
   arrowParens: 'avoid',
-  bracketSameLine: true,
   bracketSpacing: false,
+  endOfLine: 'auto',
+  htmlWhitespaceSensitivity: 'css',
+  jsxBracketSameLine: false,
+  jsxSingleQuote: true,
+  printWidth: 100,
+  proseWrap: 'preserve',
+  quoteProps: 'as-needed',
+  semi: true,
   singleQuote: true,
+  tabWidth: 2,
   trailingComma: 'all',
+  useTabs: false,
+  parser: '',
+  filepath: '',
+  rangeStart: 0,
+  rangeEnd: Infinity,
+  overrides: [
+    {
+      files: '*.json',
+      options: {
+        printWidth: 200,
+      },
+    },
+  ],
 };
